3f2e748eae31300e31f5d7d6523103f672b1ec97
Leo <leonardo2000_mx@hotmail.com>
2020-07-04 14:49:02 -0500

    Creacion de tablas entidad relacion

---
+++ sectorfinanciero/acciones/models.py
@@ -0,0 +1,37 @@
+from django.db import models
+
+# Create your models here.
+
+class Sector(models.Model):
+    nombre = models.CharField(max_length=100)
+    ticker =  models.CharField(max_length=200)
+
+class Empresa(models.Model):
+    nombre = models.CharField(max_length=200)
+    ticker =  models.CharField(max_length=200)
+    nacionalidad =  models.CharField(max_length=100)
+    descripcion =  models.TextField()
+    sector = models.ForeignKey(Sector,on_delete=models.SET_NULL, null=True)
+
+class PrecioEmpresa(models.Model):
+    precio_apertura = models.FloatField()
+    precio_cierre = models.FloatField()
+    precio_maximo = models.FloatField()
+    precio_minimo = models.FloatField()
+    fecha = models.DateField()
+    empresa =  models.ForeignKey(Empresa,on_delete=models.CASCADE)
+
+class PrecioSector(models.Model):
+    precio_apertura = models.FloatField()
+    precio_cierre = models.FloatField()
+    precio_maximo = models.FloatField()
+    precio_minimo = models.FloatField()
+    fecha = models.DateField()
+    sector =  models.ForeignKey(Sector,on_delete=models.CASCADE)
+
+
+
+
+
+    
+

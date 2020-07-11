a79177096730af6f651ecb76fe24dc94b5f4d9e0
Leo <leonardo2000_mx@hotmail.com>
2020-07-11 13:51:25 -0500

    inclusión de RestFramework y creación del Router

--- sectorfinanciero/sectorfinanciero/urls.py
+++ sectorfinanciero/sectorfinanciero/urls.py
@@ -13,9 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from rest_framework import routers
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+router = routers.DefaultRouter()
 
 urlpatterns = [
+    path('api/', include(router.urls)),
     path('admin/', admin.site.urls),
 ]
